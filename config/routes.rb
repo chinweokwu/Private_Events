d5e0832b76fc7c678c4ce28cf4592bccb68c841e
chinweokwu <paulcmorah@gmail.com>
2020-09-01 16:54:48 +0100

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :events
-  root 'events#index'
+  get 'events/index'
+  root 'home#index'
 end
