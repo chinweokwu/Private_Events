889c6b0428038e36d1119c4a1c15d614aaef2fc2
chinweokwu <paulcmorah@gmail.com>
2020-09-02 01:11:26 +0100

    added event attendee

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, :foreign_key => :creator_id
+  has_many :attended_events,  :through => :event_attendees
+has_many :event_attendees,  :foreign_key => :attendee_id
 end
---
+++ spec/requests/event_attendees_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "EventAttendees", type: :request do
+
+end
