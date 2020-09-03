87799d161a294b7e252c3650b98a713852032feb
chinweokwu <paulcmorah@gmail.com>
2020-09-03 03:29:07 +0100

    fixed event attendee

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :events, :foreign_key => :creator_id
+  has_many :events, dependent: :destroy, :foreign_key => :creator_id
   has_many :event_attendees,  :foreign_key => :attendee_id
   has_many :attended_events,  :through => :event_attendees
 
+  def attending?(event)
+    event.attendees.include?(self)
+  end
+  
+  def attend!(event)
+    self.event_attendees.create!(attended_event_id: event.id)
+  end
+  
+  def cancel!(event)
+    self.event_attendees.find_by(attended_event_id: event.id).destroy
+  end
 end
