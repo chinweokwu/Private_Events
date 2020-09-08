23a5f2cf014242fecbd7eebbd0bdc4cc32da33fb
chinweokwu <paulcmorah@gmail.com>
2020-09-08 19:57:01 +0100

    added more logic to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,33 +5,35 @@ module ApplicationHelper
 
   def future_attended_events(user)
     future_events = @attended_events.future
-end
+  end
 
-def current_user?(user)
+  def current_user?(user)
     current_user == user
-end
-
-def notification(notice, alert)
-  if notice
-    content_tag(:p, notice, class: 'notice notification is-primary global-notification')
-  elsif alert
-    content_tag(:p, alert, class: 'alert notification is-danger global-notification')
   end
-end
 
-def show_user
-  if user_allowed
-    (link_to 'Edit', edit_event_path(@event)) +
-    (link_to 'Destroy', @event, method: :delete, data: { confirm: 'Are you sure?' })
+  def notification(notice, alert)
+    if notice
+      content_tag(:p, notice, class: 'notice notification is-primary global-notification')
+    elsif alert
+      content_tag(:p, alert, class: 'alert notification is-danger global-notification')
+    end
   end
-end
 
-def signed_in_user
-  if user_signed_in?
-    render partial: signed_in
-  else
-    render partial: signed_out
+  def show_user
+    if user_allowed
+      (link_to 'Edit', edit_event_path(@event)) +
+      (link_to 'Destroy', @event, method: :delete, data: { confirm: 'Are you sure?' })
+    end
   end
-end
+
+  def login_helper(current_user)
+    if user_signed_in?     
+      render :partial => 'layouts/signed_in'
+    else
+      (link_to 'Events', events_path , class:"navbar-item")+
+      (link_to "Sign In", new_user_session_path, class:"navbar-item")+
+      (link_to "Sign up", new_user_registration_path, class:"navbar-item")
+    end
+  end
 
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,25 @@
 module EventsHelper
+    def attend_event(current_user)
+        if user_signed_in? && @event.date  > Date.today
+            render :partial => 'events/attend_event'
+        end
+    end
+
+    def attend(current_user)
+        if user_signed_in? && current_user.attending?(@event) 
+            render :partial => 'events/cancel_event'
+	    else
+            render :partial => 'events/attend_event'
+        end
+    end
+
+    def attendee
+        if @event.attendees.any? 
+            @event.attendees.each do |attendee|
+             attendee.username
+            end
+        else 
+            No attendees
+        end
+    end
 end
