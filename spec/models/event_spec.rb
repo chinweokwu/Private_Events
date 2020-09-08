c821996de374385833a884566c79e021871431e7
Laguna1 <lagunazoomarket@gmail.com>
2020-09-08 23:41:24 +0300

    add event_controller test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   describe 'associations' do
     it { should belong_to(:creator).class_name('User') }
-   it { should have_many(:event_attendees) }
+    it { should have_many(:event_attendees) }
   end
 
   describe 'validations' do
@@ -12,4 +12,31 @@ RSpec.describe Event, type: :model do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:date) }
   end
+
+  subject{described_class.new(location:'nil',description:'nil',date:'nil')}
+  
+    it 'is invalid without a location' do
+       subject.location = 'nil'      
+    end
+  
+   it 'is invalid without a description' do
+      subject.description = 'nil'      
+  end
+  
+  it 'is invalid without a date' do
+    subject.date = 'nil'
+  end 
+  
+  it 'is valid with a location' do
+    subject.location = 'Anything'
+  end
+  
+  it 'is valid with a description' do
+    subject.description = 'Anything'
+  end 
+  
+  it 'is valid with a date' do
+    subject.date = '10/10/2020'
+  end
+
 end
