c893a7e31e267caf78db0380a00003d3a79ceb94
Laguna1 <lagunazoomarket@gmail.com>
2020-09-10 01:23:44 +0300

    add tests

--- spec/views/home/index.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'home/index.html.erb', type: :view do
-  scenario 'home page' do
-    visit '/signup'
-    expect(page).to have_content('Sign up | Private Events')
-  end
-end
