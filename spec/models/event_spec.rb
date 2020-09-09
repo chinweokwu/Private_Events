4b9a3df6143b0be955c22a1764c0ab82ebc75555
chinweokwu <paulcmorah@gmail.com>
2020-09-09 22:06:59 +0100

    tested event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(name: "paul oksana",
+                        location: "nigeria ukraine",
+                        description: "Lorem ipsum",
+                        date: Time.now,
+                        user_id: 1)
+  }
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "is not valid without a location" do
+    subject.location = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a date" do
+    subject.date = nil
+    expect(subject).to_not be_valid
+  end
 end
