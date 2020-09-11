7c3b5189b2fcdfd81bdfbe0d57810712b737c187
Laguna1 <lagunazoomarket@gmail.com>
2020-09-12 01:17:16 +0300

    add event model validations test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  describe 'associations' do
+    it { should belong_to(:creator).class_name('User') }
+    it { should have_many(:event_attendees) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:location) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:date) }
+  
+
+  end
+
   let(:creator) { User.new }
   subject do
     Event.new(name: 'Birthday',
