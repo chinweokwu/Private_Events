6482f984c107e2488717b4b7f8e5b442bba76938
chinweokwu <paulcmorah@gmail.com>
2020-09-11 05:13:57 +0100

    added changes to home spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Home features' do
-  it 'displays home page' do
+  it 'displays home & event pages' do
     visit('/home/index')
     expect(page).to have_content('Home#index')
     click_link('Events')
